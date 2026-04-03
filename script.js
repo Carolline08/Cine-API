6306a69e083c4a80b463483fc37fd22ade235a97
Carolline Barbosa <carollinebarbosa225@gmail.com>
2026-04-03 09:59:16 -0300

    Atualizando site com a nova versão

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
-document.getElementById("btnSortear")
-.addEventListener("click", getFilmeseSeries);
+document.addEventListener("DOMContentLoaded", () => {
 
-async function getFilmeseSeries() {
-    try {
-        const response = await fetch("https://api.tvmaze.com/search/shows?q=star");
-        const data = await response.json();
+    const btn = document.getElementById("btnSortear");
 
-        const sorteio = data[Math.floor(Math.random() * data.length)];
-        const serie = sorteio.show;
+    btn.addEventListener("click", function(e) {
 
-        // remove HTML da descrição
-        const resumoLimpo = serie.summary.replace(/<[^>]*>/g, '');
+        
+        const circle = document.createElement("span");
 
-        document.getElementById("filmesSeries").innerHTML = `
-            <h3>${serie.name}</h3>
-            <img src="${serie.image?.medium}" width="200">
-            <p>${resumoLimpo}</p>
-        `;
-    } catch (err) {
-        console.log("Erro:", err);
+        const size = Math.max(this.clientWidth, this.clientHeight);
+
+        circle.style.width = circle.style.height = size + "px";
+        circle.style.left = e.offsetX - size / 2 + "px";
+        circle.style.top = e.offsetY - size / 2 + "px";
+
+        this.appendChild(circle);
+
+        setTimeout(() => circle.remove(), 600);
+
+        
+        getFilmeseSeries();
+    });
+
+    async function getFilmeseSeries() {
+        try {
+            if (navigator.vibrate) {
+                navigator.vibrate(200);
+            }
+
+            const div = document.getElementById("filmesSeries");
+            div.style.display = "block";
+            div.innerHTML = "Carregando... ⏳";
+
+            const response = await fetch("https://api.tvmaze.com/search/shows?q=star");
+            const data = await response.json();
+
+            const sorteio = data[Math.floor(Math.random() * data.length)];
+            const serie = sorteio.show;
+
+            const resumoLimpo = serie.summary.replace(/<[^>]*>/g, '');
+
+            
+            document.body.style.setProperty("--bg", `url(${serie.image?.original})`);
+
+            div.innerHTML = `
+                <h3>${serie.name}</h3>
+                <img src="${serie.image?.medium}">
+                <p>${resumoLimpo}</p>
+            `;
+
+        } catch (err) {
+            console.log(err);
+        }
     }
-}
+
+});
